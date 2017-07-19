9f009ec9818da6a45b4a39672aa4653e78a64450
Araya.Kunal <Araya.Kunal@ArayaKunal-PC>
2017-07-20 01:32:39 +0530

    new code

---
+++ src/com/eva/test/JavaDictionary.java
@@ -0,0 +1,17 @@
+package com.eva.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JavaDictionary {
+
+	public static void main(String[] args) {
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Name", "Kunal Goutam Das");
+		System.out.println(map.get("Name"));
+		
+		
+
+	}
+}
